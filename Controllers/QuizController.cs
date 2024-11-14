2e485655ca513e6c0730b7b16444cd6a99fe81a0
=AAGoitia701 <aagoitia701@gmail.com>
2024-11-14 12:49:44 +0100

    Made used of tempdata to save the list of user answers to show later, and added static var for the number of correct answers, also created a viewmodel to be able to return both on the same method, used Jsonconvert to serialize and deserialize the tempdata, otherwise it gives a null

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -11,7 +11,7 @@ namespace ConsumingAPI.Controllers
     public class QuizController : Controller
     {
         private static int currentId = 2;
-        private static int correctAnswers =0;
+        private static int correctAnswers = 0;
         Uri uri = new Uri("https://localhost:7052/");
         private readonly HttpClient _httpClient;
 
@@ -84,7 +84,7 @@ namespace ConsumingAPI.Controllers
                 quizobj = JsonConvert.DeserializeObject<Quiz>(data);
             }
 
-            if (currentId > 3)
+            if (currentId > 3) //change it to .count()
             {
                 //Console.WriteLine($"You had {correctAnswers} correct answers");
                 //RedirectToAction("FinishedQuiz");
@@ -114,35 +114,53 @@ namespace ConsumingAPI.Controllers
 
        public ActionResult CheckAnswer(string answer, string correctAnswer) 
        {
+            List<string> answerList = new List<string>(); //used because tempdata does not allow complex types
             if (ModelState.IsValid) 
             {
-                CorrectIncorrectViewModel correctIncorrectViewModel = new CorrectIncorrectViewModel();
-
-
                 if (correctAnswer.ToLower().Contains(answer.ToLower()))
                 {
                     correctAnswers++;
-                    correctIncorrectViewModel.CorrectAnswers++;
                 }
                 else
                 {
-                    correctIncorrectViewModel.IncorrectAnswers.Add(answer);
+                    answerList.Add(answer);
                 }
 
-   
-            }          
-            
+
+            }
+            TempData["answerUserList"] = JsonConvert.SerializeObject(answerList); //serialize the list, so it can be used later
 
             return RedirectToAction("GetOneViewModel");
        }
 
         public ActionResult FinishedQuiz()
         {
-       
+            // Makes sure tempdata has values before using it
+            if (TempData.ContainsKey("answerUserList"))
+            {
+                // Keep the data 
+                TempData.Keep("answerUserList"); // Keeps "answerUserList" in TempData
 
-            return View();
+                var listAnswersJson = TempData["answerUserList"] as string;
 
-           
+                var answerList = JsonConvert.DeserializeObject<List<string>>(listAnswersJson); //deserialize the string to a list
+
+                //used viewmodel to be able to send more data to view
+                CorrectIncorrectViewModel correctIncorrects = new CorrectIncorrectViewModel()
+                {
+                    CorrectAnswers = correctAnswers,
+                    IncorrectAnswers = answerList
+                };
+
+                return View(correctIncorrects);
+            }
+            else
+            {
+                // if tempdata has no data
+                return View(new List<string>()); // sends empty list
+            }
+
+
 
         }
     }
--- obj/Debug/net8.0/ConsumingAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsumingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsumingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017d142398609dea9609e09c83b168f160bc9e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb9a93ea612941df92130ffd3a117419f1bd673")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsumingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsumingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
